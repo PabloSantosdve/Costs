7b8a6074c364cabb7fae042caccc13559e7b9b4d
PabloSantosdve <148356304+PabloSantosdve@users.noreply.github.com>
2024-09-03 12:02:59 -0300

    atualizando o projeto

--- Custos/src/App.jsx
+++ Custos/src/App.jsx
@@ -1,31 +1,32 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import Home from './components/pages/Home'
-import Contact from './components/pages/Contact'
-import Company from './components/pages/Company'
-import NewProject from './components/pages/NewProject'
-
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+import Container from './components/pages/layout/Container';
 
 function App() {
-
   return (
     <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/newproject">New project</Link>
+      </div>
 
-          <div>
-            <Link to="/">Home</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/company">Company</Link>
-            <Link to="/newproject">New project</Link>
-          </div>
-
+      <Container customClass="min-height">
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/company' element={<Company />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/newproject' element={<NewProject />} />
         </Routes>
-        <p>Footer</p>
+      </Container>
+
+      <p>Footer</p>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- Custos/src/components/pages/layout/Container.jsx
+++ Custos/src/components/pages/layout/Container.jsx
@@ -1,8 +1,21 @@
-import styles from './Container.module.css'
+// Importa o módulo de estilos CSS do arquivo Container.module.css
+import styles from './Container.module.css';
 
-
-function Container(props){
-	return <div>{props.children}</div>
+// Define o componente funcional Container
+function Container(props) {
+	// Retorna o JSX para renderização
+	return (
+		// Cria um div com classes de estilo dinâmicas
+		<div className={`${styles.container} ${styles[props.customClass]}`}>
+			{/* Renderiza o conteúdo filho passado para o Container */}
+			{props.children}
+		</div>
+	);
 }
 
-export default Container
+// Exporta o componente Container para uso em outros arquivos
+export default Container;
+
+
+
+//CLASSE DINÂMINCA
