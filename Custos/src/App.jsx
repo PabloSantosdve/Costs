758afc8e18f4d60b38fca03b3e0291a0b9ad48e4
PabloSantosdve <148356304+PabloSantosdve@users.noreply.github.com>
2024-09-09 14:58:54 -0300

    Finalizando o NavBar

--- Custos/src/App.jsx
+++ Custos/src/App.jsx
@@ -3,28 +3,28 @@ import Home from './components/pages/Home';
 import Contact from './components/pages/Contact';
 import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
-import Container from './components/pages/layout/Container';
+import Container from './components/layout/Container';
+import NavBar from './components/layout/NavBar'
+import Footer from './components/layout/Footer'
+import Projects from './components/pages/Projects';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/company">Company</Link>
-        <Link to="/newproject">New project</Link>
-      </div>
+        <NavBar />
 
       <Container customClass="min-height">
+
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
           <Route path='/company' element={<Company />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/newproject' element={<NewProject />} />
         </Routes>
       </Container>
 
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
